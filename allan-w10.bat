98d6ebf9afacc10c536f3277bfc180f060fc0863
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2020-07-24 17:12:18 -0300

    Process FILE_LIST from .txt file
    One full path per line from {bat-filename}.txt

--- allan-w10.bat
+++ allan-w10.bat
@@ -1,15 +1,18 @@
 @echo off
-set FILE_LIST=( %WINDIR%\system32\CompatTelRunner.exe ^
-                %WINDIR%\system32\MRT.exe ^
-                %WINDIR%\HelpPane.exe )
-for %%f in %FILE_LIST% do (
-	echo %%f
-	if exist %%f (
-		taskkill /f /im %%~nxf
-		takeown /f %%f
-		icacls %%f /deny *S-1-1-0:(X^)
-		echo ^> blocked
-		echo.
+setlocal
+set FILE_LIST=%~dp0%~n0.txt
+if exist %FILE_LIST% (
+	for /f "delims=" %%f in (%FILE_LIST%) do (
+		echo %%f
+		if exist %%f (
+			taskkill /f /im %%~nxf
+			takeown /f %%f
+			icacls %%f /deny *S-1-1-0:(X^)
+			echo ^> blocked
+			echo.
+		)
 	)
+) else (
+	echo "%FILE_LIST%" was not found
 )
 REM pause
